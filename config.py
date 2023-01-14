a567d49e6aaba533ce4363f18d9f1b0c0e58dd6c
DreamFeat <100403640+vaskinsergey35775@users.noreply.github.com>
2023-01-14 18:08:46 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ command_prefixes = ['.','!','/']
 
 ghoul_table_command = 'ghoul-c'
 
-end_message = 'l l let me die' # Сообщение после конца цикла, если не нужно - оставляем пустым
-messages_per_second = 7 # Для ghoul_spam
+end_message = 'Илья долбаёб, сделано пидорасом @dreamfeat' # Сообщение после конца цикла, если не нужно - оставляем пустым
+messages_per_second = 10 # Для ghoul_spam
 sleep_time_ghoul = 0.1 # Для ghoul_table 0.0825
 
 # Линии ниже не трогать, сломается!
